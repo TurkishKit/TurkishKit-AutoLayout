0828d86380c46b572ef8757eec61bcb7f84f88c7
Nadin Tamer <nadin.tamer@gmail.com>
2019-05-02 20:25:43 +0300

    Initial commit

---
+++ TurkishKit-AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurkishKit-AutoLayout
+//
+//  Created by Nadin Tamer on 2.05.2019.
+//  Copyright © 2019 Nadin Tamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
